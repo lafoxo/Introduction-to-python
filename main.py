d0eebc294d3335193b97d6edc6308c1e049a565f
lafoxo <info4lolo6@gmail.com>
2024-12-07 17:31:46 +0100

    commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#hello world
+print("hello,world")
+#that is my actual name
+print("my name is ololade")
+#thats kinda true i love sprunki instaed
+print("i love playing soceer")
+#calculator
+print("the result of 3-5=",3-5)
+#calculator again
+print("the result of 999*7=",999*7)
+#calculator one last toime
+print("the result of 500/5=",500/5)
+#this is actually pytho yay
+print("this is python")
+#this is good
